d2a14ed887764a8b32b6545aaceb562cae852009
현석 송 <jhn06046@hotmail.com>
2020-10-08 09:27:39 +0900

    템플릿 엔진

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // express를 실행할 경우 두 가지 약속을 정해둔 것
 const express = require('express');
 let app = express();
+// jade의 소스를 pretty하게 변경하는 소스
+app.locals.pretty = true;
 
-// nodeJs에서 직접 웹서버를 만들었던 것 처럼 3000번 포트를 listen하게 함
-app.listen(3000, ()=>{
-    console.log('Connected:3000 port!');
-});
+// app에 템플릿 엔진을 사용한다고 알려줌
+app.set('view engine','jade');
+// 템플릿 파일은 views에 넣음
+app.set('views','./views');
+
 app.use(express.static('public'));
+
+// /template에 소스코드를 가져와서 웹페이지를 만드는 랜더링을 사용
+app.get('/template',(req,res)=>{
+    // temp라는 페이지를 rendering해서 전송한다.
+    res.render('temp',{time:Date(), title:'Jade'});
+})
 
 // 홈페이지에 접속했을 때 홈페이지에 접속했다는 것을 나타낸다.
 // 접속할 경우 get/post 방식을 사용, url을 직접 치는 방식은 get방식
@@ -46,3 +55,9 @@ app.get('/dynamic',(req, res)=>{
     res.send(output);
 })
 
+
+
+// nodeJs에서 직접 웹서버를 만들었던 것 처럼 3000번 포트를 listen하게 함
+app.listen(3000, ()=>{
+    console.log('Connected:3000 port!');
+});
